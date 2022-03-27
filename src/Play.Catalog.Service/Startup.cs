b9059193c798e762fb637439c67ec8c740b7682b
SrAlch94 <daniel.contero@gmail.com>
2022-03-28 00:35:07 +0100

    Refactoring mongoDB entity connection to prep for nuget

--- src/Play.Catalog.Service/Startup.cs
+++ src/Play.Catalog.Service/Startup.cs
@@ -15,6 +15,8 @@ using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
 using MongoDB.Driver;
+using Play.Catalog.Service.Entities;
+using Play.Catalog.Service.Repositories;
 using Play.Catalog.Service.Settings;
 
 namespace Play.Catalog.Service
@@ -34,21 +36,13 @@ namespace Play.Catalog.Service
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            
-            BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
-            BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
 
             serviceSettings = Configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
 
-            services.AddSingleton(serviceProvider => {
-                var mongoDbSettings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
-                var mongoClient = new MongoClient(mongoDbSettings.ConnectionString);
-                return mongoClient.GetDatabase(serviceSettings.ServiceName);
-            });
+            services.AddMongo()
+                        .AddMongoRepository<Item>("items");
 
-            services.AddSingleton<IItemsRepository, ItemsRepository>();
-
-            services.AddControllers(options => 
+            services.AddControllers(options =>
             {
                 options.SuppressAsyncSuffixInActionNames = false;
             });
