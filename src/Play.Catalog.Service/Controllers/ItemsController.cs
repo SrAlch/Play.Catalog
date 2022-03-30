29942cfe5eae693f6ad1f634ce717b37d482dba7
SrAlch94 <daniel.contero@gmail.com>
2022-03-30 22:16:17 +0100

    nuget package gen

--- src/Play.Catalog.Service/Controllers/ItemsController.cs
+++ src/Play.Catalog.Service/Controllers/ItemsController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Play.Catalog.Service.Dtos;
 using Play.Catalog.Service.Entities;
+using Play.Common;
 
 namespace Play.Catalog.Service.Controllers
 {
